3e2dafd7eeed2f04306cc27b653bab7e8df96e55
Landon Lloyd <65516863+LandonLloyd@users.noreply.github.com>
2020-07-11 15:13:16 -0600

    Update Sum All Primes.js

--- Intermediate Algorithm Scripting/Sum All Primes.js
+++ Intermediate Algorithm Scripting/Sum All Primes.js
@@ -1,3 +1,7 @@
+//Instructions from FreeCodeCamp:
+/*A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. 
+In contrast, 4 is not prime since it is divisible by 1, 2 and 4. Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.*/
+
 function sumPrimes(num) {
   const prime = [2]
 for(let i = 3; i <= num; i++){ //just change to this :)
