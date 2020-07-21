f53617dea6a1738ca4d78fe4b55e2d2f8e198bd9
nhcarrigan <nhcarrigan@gmail.com>
2020-07-21 11:41:46 -0700

    Restructure files

---
+++ FCC Intermediate Algorithm Scripting/Make A Person.js
@@ -0,0 +1,40 @@
+//Instructions from FreeCodeCamp:
+/*Fill in the object constructor with the following methods below:
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must 
+be the only available means of interacting with the object.*/
+
+var Person = function(firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  var name = firstAndLast.split(' ')
+  this.getFullName = function() {
+    return name.join(' ');
+  };
+  this.getFirstName = function(){
+    return name[0];
+  }
+  this.getLastName = function(){
+    return name[1]
+  }
+  this.setFirstName = function(first){
+    return name[0] = first;
+  }
+  this.setLastName = function(last){
+    return name[1] = last;
+  }
+  this.setFullName = function(full){
+    return name = full.split(' ');
+
+  }
+console.log(firstAndLast.split(' ')[0])
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
+
---
+++ FCC Intermediate Algorithm Scripting/Sum All Primes.js
@@ -0,0 +1,26 @@
+//Instructions from FreeCodeCamp:
+/*A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. 
+In contrast, 4 is not prime since it is divisible by 1, 2 and 4. Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.*/
+
+function sumPrimes(num) {
+  const prime = [2]
+for(let i = 3; i <= num; i++){ //just change to this :)
+  if(isPrime(i)){
+    prime.push(i)
+  }
+}
+
+  console.log(prime.reduce((num,prime) => num + prime))
+  return prime.reduce((num, prime) => num + prime)
+}
+
+sumPrimes(10);
+
+function isPrime(num){
+  for(let j = 2; j < num; j++){
+      if(num % j == 0){
+        return false;
+        
+      }
+    }return true;
+}
