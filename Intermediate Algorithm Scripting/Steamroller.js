92ed7b1fce478cc682be0c183145d41cb6503bfb
nhcarrigan <nhcarrigan@gmail.com>
2020-07-21 11:41:57 -0700

    Restructure files

--- Intermediate Algorithm Scripting/Steamroller.js
+++
@@ -1,9 +0,0 @@
-//Instructions from FreeCodeCamp:
-//Flatten a nested array. You must account for varying levels of nesting.
-
-steamrollArray([1, [2], [3, [[4]]]], Infinity);
-function steamrollArray(arr, d = 5) {
-   return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? steamrollArray(val, d - 1) : val), []) : arr.slice();
-}
-
-steamrollArray([1, [2], [3, [[4]]]], Infinity);
