91ba1a2b094f71e7f8bd2b50c963927f1ea4ba30
Landon Lloyd <65516863+LandonLloyd@users.noreply.github.com>
2020-07-25 17:59:35 -0600

    Create  Stop gninnipS My sdroW!.js

---
+++ Codewars/Stop gninnipS My sdroW!.js
@@ -0,0 +1,17 @@
+//Instructions from Codewars:
+/*Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this 
+Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present. Examples: spinWords( "Hey fellow 
+warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona 
+test"*/
+
+function spinWords(){
+  const args = [...arguments]
+  let split = args[0].split(' ');
+  console.log(split)
+  for(let i = 0; i < split.length; i++){
+    if(split[i].length >= 5){
+    split[i] = split[i].split('').reverse().join('')
+    }
+  }
+  return split.join(' ');
+}
